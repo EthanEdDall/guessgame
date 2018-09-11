3c9e35bb1005bca7cdb29ab765de67777e20ee02
Michael Ranciglio <michael.ranciglio@wwinc.com>
2018-09-11 14:16:00 -0500

    Make it a guessing game

--- guessgame.py
+++ guessgame.py
@@ -9,5 +9,13 @@ import random
 MIN = 1
 MAX = 20
 
-targetNumber = random.randint(MIN, MAX)
-print(targetNumber)
+target_number = random.randint(MIN, MAX)
+print("Guess a number between", MIN, "and", MAX,":")
+
+user_guess = int(input().strip())
+
+if user_guess == target_number:
+    print("You win!")
+else:
+    print("You suck!")
+    print("The correct answer was:", target_number)
